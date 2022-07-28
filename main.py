32be50b9c601cee4ac74f3718dbbc807dbe30e61
shadatm <shadat.rahman.464@gmail.com>
2022-07-29 01:02:01 +0600

    added router

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from time import sleep
-from typing import List
-from fastapi import Depends, FastAPI, Response, status, HTTPException
+from fastapi import FastAPI
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from requests import Session
-from . import models, schemas
-from . import utils
-from sqlalchemy.orm import Session
-from .database import engine, get_db
+from . import models
+from .database import engine
+from .routers import post, user
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -26,87 +23,10 @@ while True:
         print("Connection failed!!!")
         sleep(2)
 
-
-@app.get("/posts", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts;""")
-    # posts = cursor.fetchall()
-
-    posts = db.query(models.Post).all()
-    return posts
+app.include_router(post.router)
+app.include_router(user.router)
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_post(post: schemas.PostCreate, response: Response, db: Session = Depends(get_db)):
-    # cursor.execute(
-    #     """INSERT INTO posts (title, content) VALUES (%s, %s) RETURNING *""", (post.title, post.content))
-    # new_post = cursor.fetchone()
-    # conn.commit()
-    new_post = models.Post(**post.dict())
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-
-    return new_post
-
-
-@app.get("/posts/{id}", response_model=schemas.Post)
-def get_post(id: int, response: Response, db: Session = Depends(get_db)):
-
-    # cursor.execute("""SELECT * FROM posts WHERE id=%s""", (str(id),))
-    # data = cursor.fetchone()
-    data = db.query(models.Post).filter(models.Post.id == id).first()
-    if not data:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id {id} was not found.")
-    #     response.status_code = status.HTTP_404_NOT_FOUND
-    return data
-
-
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
-    # post = cursor.execute(
-    #     """DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
-    # post = cursor.fetchone()
-    # conn.commit()
-
-    post = db.query(models.Post).filter(models.Post.id == id)
-
-    if post.first() == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id {id} was not found")
-    post.delete(synchronize_session=False)
-    db.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-@app.put("/posts/{id}", status_code=status.HTTP_200_OK, response_model=schemas.Post)
-def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
-    # cursor.execute("""UPDATE posts SET title = %s, content = %s WHERE id = %s RETURNING *""",
-    #                (post.title, post.content, str(id),))
-    # updated_post = cursor.fetchone()
-    # conn.commit()
-
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-    post = post_query.first()
-
-    if post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id {id} was not found")
-    post_query.update(updated_post.dict(), synchronize_session=False)
-    db.commit()
-    return post_query.first()
-
-
-@app.post('/users', status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-
-    # hash the password
-    hashed_password = utils.hash(user.password)
-    user.password = hashed_password
-
-    new_user = models.User(**user.dict())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+@app.get("/")
+def root():
+    return {"Hello": "WELCOME TO THE API WORLD"}
