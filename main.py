4d8c985b5e5be6fcbca02d3be35ff9f0e5f81be8
shadatm <shadat.rahman.464@gmail.com>
2022-07-28 14:41:48 +0600

    converted SQL code to pure python code using sqlAlchemy

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from os import sync
 from time import sleep
 from fastapi import Depends, FastAPI, Response, status, HTTPException
 import psycopg2
@@ -26,39 +27,35 @@ while True:
         sleep(2)
 
 
-# def find_post(id):
-#     for p in my_posts:
-#         if p["id"] == id:
-#             return p
-
-
-# def find_index(id):
-#     for i, p in enumerate(my_posts):
-#         if p["id"] == id:
-#             return i
-
-
 @app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM posts;""")
-    posts = cursor.fetchall()
+def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts;""")
+    # posts = cursor.fetchall()
+
+    posts = db.query(models.Post).all()
     return {"data": posts}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: schemas.PostCreate, response: Response):
-    cursor.execute(
-        """INSERT INTO posts (title, content) VALUES (%s, %s) RETURNING *""", (post.title, post.content))
-    new_post = cursor.fetchone()
-    conn.commit()
+def create_post(post: schemas.PostCreate, response: Response, db: Session = Depends(get_db)):
+    # cursor.execute(
+    #     """INSERT INTO posts (title, content) VALUES (%s, %s) RETURNING *""", (post.title, post.content))
+    # new_post = cursor.fetchone()
+    # conn.commit()
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+
     return {"data": new_post}
 
 
 @app.get("/posts/{id}")
-def get_post(id: int, response: Response):
+def get_post(id: int, response: Response, db: Session = Depends(get_db)):
 
-    cursor.execute("""SELECT * FROM posts WHERE id=%s""", (str(id),))
-    data = cursor.fetchone()
+    # cursor.execute("""SELECT * FROM posts WHERE id=%s""", (str(id),))
+    # data = cursor.fetchone()
+    data = db.query(models.Post).filter(models.Post.id == id).first()
     if not data:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id {id} was not found.")
@@ -67,29 +64,38 @@ def get_post(id: int, response: Response):
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    post = cursor.execute(
-        """DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
-    post = cursor.fetchone()
-    conn.commit()
-    if post == None:
+def delete_post(id: int, db: Session = Depends(get_db)):
+    # post = cursor.execute(
+    #     """DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
+    # post = cursor.fetchone()
+    # conn.commit()
+
+    post = db.query(models.Post).filter(models.Post.id == id)
+
+    if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id {id} was not found")
-
+    post.delete(synchronize_session=False)
+    db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.put("/posts/{id}", status_code=status.HTTP_200_OK)
-def update_post(id: int, post: schemas.PostCreate):
-    cursor.execute("""UPDATE posts SET title = %s, content = %s WHERE id = %s RETURNING *""",
-                   (post.title, post.content, str(id),))
-    updated_post = cursor.fetchone()
-    conn.commit()
-    if updated_post == None:
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
+    # cursor.execute("""UPDATE posts SET title = %s, content = %s WHERE id = %s RETURNING *""",
+    #                (post.title, post.content, str(id),))
+    # updated_post = cursor.fetchone()
+    # conn.commit()
+
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id {id} was not found")
-
-    return {"data": updated_post}
+    post_query.update(updated_post.dict(), synchronize_session=False)
+    db.commit()
+    return {"data": post_query.first()}
 
 # Added this comment from manjaro
 # Added this comment from windows11
