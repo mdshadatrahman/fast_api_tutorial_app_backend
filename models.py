1c0d41245a651193dfb2ad899efbf45146723848
shadatm <shadat.rahman.464@gmail.com>
2022-07-28 16:49:05 +0600

    created user model

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+from enum import unique
+from tkinter.tix import Tree
 from sqlalchemy import TIMESTAMP, Boolean, Column, Integer, String, text
 from .database import Base
 
@@ -10,3 +12,12 @@ class Post(Base):
     published = Column(Boolean, server_default='True', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         server_default=text("NOW()"), nullable=False)
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False,  server_default=text("NOW()"))
