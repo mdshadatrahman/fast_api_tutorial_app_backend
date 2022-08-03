edb0e1604fc1f82c7126e9481d7ed7f15a885e5c
shadatm <shadat.rahman.464@gmail.com>
2022-08-04 01:56:40 +0600

    asdf

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 import models
 from database import engine
-from .routers import post, user, auth, vote
+import routers
+# from routers import post, user, auth, vote
 from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
@@ -18,10 +19,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(post.router)
-app.include_router(user.router)
-app.include_router(auth.router)
-app.include_router(vote.router)
+app.include_router(routers.post.router)
+app.include_router(routers.user.router)
+app.include_router(routers.auth.router)
+app.include_router(routers.vote.router)
 
 
 @app.get("/")
