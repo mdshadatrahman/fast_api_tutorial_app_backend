a444f21decd297b936078d9d6f4fd00a257de680
shadatm <shadat.rahman.464@gmail.com>
2022-08-04 01:28:23 +0600

    asdf

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from .models import Base
-from .database import engine
-from .routers import post, user, auth, vote
+from models import Base
+from database import engine
+from routers import post, user, auth, vote
 from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
